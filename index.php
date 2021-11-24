dd03da46c152ebfe4f7f8741c633d8eded83982a
1Y <arseniy.krugloff@gmail.com>
2021-11-24 21:30:47 +0300

    Commit

--- index.php
+++ index.php
@@ -1,31 +1,24 @@
 <?
-if ($_SERVER['REQUEST_URI'] == '/')
+include 'PHP/Database.php';
+    
+$URL = explode('/', parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))[1];
+$User = $SQL->query("SELECT * FROM `users` WHERE (`Link_FullAccess` = '$URL') OR (`Link_ReadOnly` = '$URL') OR (`Link_TimetableOnly` = '$URL')");
+
+if ($User->num_rows === 1)
 {
-    include 'Landing/Index.html';
+    $User = $User->fetch_row();
+
+    if ($User[1] === $URL)
+        $AccessLevel = 2;
+    else if ($User[2] === $URL)
+        $AccessLevel = 1;
+    else if ($User[3] === $URL)
+        $AccessLevel = 0;
+
+    include 'MainSequence.php';
 }
 else
 {
-    include 'PHP/Database.php';
-    
-    $URL = explode('/', parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))[1];
-    $User = $SQL->query("SELECT * FROM `users` WHERE (`Link_FullAccess` = '$URL') OR (`Link_ReadOnly` = '$URL') OR (`Link_TimetableOnly` = '$URL')");
-    
-    if ($User->num_rows === 1)
-    {
-        $User = $User->fetch_row();
-    
-        if ($User[1] === $URL)
-            $AccessLevel = 2;
-        else if ($User[2] === $URL)
-            $AccessLevel = 1;
-        else if ($User[3] === $URL)
-            $AccessLevel = 0;
-    
-        include 'MainSequence.php';
-    }
-    else
-    {
-        http_response_code(404);
-    };
+    http_response_code(404);
 };
 ?>
